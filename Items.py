7b83332ec369cf9276f4eb886d7bc53f6ae5aef8
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2024-11-20 22:24:44 +0300

    created armor class

--- Items.py
+++ Items.py
@@ -30,4 +30,32 @@ class Weapon:
         print(f'Name: {self.__name} Attack Power: {self.__attack_power}, Durability: {self.__durability}, Critical Rate: {self.__critical_rate}')
 
 class Armor:
-    pass
+    def __init__(self, name, defence, durability, block_rate):
+        self.__name = name
+        self.__defence = defence
+        self.__durability = durability
+        self.__block_rate = block_rate
+
+    def get_name(self):
+        return self.__name
+
+    def get_defence(self):
+        return self.__defence
+
+    def set_defence(self, defence):
+        self.__defence = defence
+
+    def get_durability(self):
+        return self.__durability
+
+    def set_durability(self, durability):
+        self.__durability = durability
+
+    def get_block_rate(self):
+        return self.__block_rate
+
+    def set_block_rate(self, block_rate):
+        self.__block_rate = block_rate
+
+    def show_armor(self):
+        print(f'Name: {self.__name} Defence: {self.__defence}, Durability: {self.__durability}, Block Rate: {self.__block_rate}')
