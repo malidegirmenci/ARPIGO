d5715f4dc65535722a66cd755473e7897c7d8b75
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2024-11-20 22:31:44 +0300

    armor class tested and damage method tested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Chars import Tank, Character, GeneralMonster, UniqueMonster, SwordMaster, Archer
 from Helper import show_message
-from Items import Weapon
+from Items import Weapon, Armor
 
 print('!!!ARPIGO!!!')
 print('New Generation RPG Game')
@@ -21,14 +21,29 @@ player2.show_info()
 print(player2.damage(manter))
 
 show_message(f'{player2.get_name()}, {manter.get_name()} {player2.damage(manter)} hasar verdi. ')
-'''
 
-clericRod = Weapon('Cleric Rod', 120, 100, 2)
-swordMaster = SwordMaster('Kamil')
+clericRod = Weapon('Cleric Rod', 120, 100, 1)
+
 swordMaster.show_info()
 swordMaster.get_weapon().show_weapon()
 
 swordMaster.set_weapon(clericRod)
 swordMaster.show_info()
+swordMaster.get_weapon().show_weapon()
 
+swordMaster = SwordMaster('Kamil')
+swordMaster.show_info()
+swordMaster.get_weapon().show_weapon()
+swordMaster.get_armor().show_armor()
 
+swordMaster.set_armor()
+swordMaster.get_armor().show_armor()
+
+swordMaster.special_skill()
+'''
+swordMaster = SwordMaster('Kamil')
+swordMaster.show_info()
+manter = GeneralMonster('Manter', 100, 10,  60)
+manter.show_info()
+
+print(f'{swordMaster.get_name()}, {manter.get_name()}e {swordMaster.damage(manter)} hasar verdi.')
